d2070292ddbb45b7473884767a9372b45d204053
Anupta <anupta22@gmail.com>
2024-06-02 13:12:37 -0400

    Added problem 6 solution

---
+++ Filter-Elements-from-Array/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    filtered = [];
+    arr.forEach((element, index) => {
+        if (Boolean(fn(element, index)) == true) filtered.push(element);
+    });
+    return filtered;
+};
